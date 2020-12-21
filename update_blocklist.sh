d29e028c432310b8c60affe8ae299564fa59505e
Blue Bear <bluebear@Windows-10.local>
2020-12-21 01:37:45 -0800

    rename functions for specific list-source; fetch is subprocess; parse is subprocess; uses sort to rid of duplicates and creates smaller file to load

--- update_blocklist.sh
+++ update_blocklist.sh
@@ -15,39 +15,43 @@ CONFIG_DIR="/var/unbound/conf.d/block"
 # Temp directory inside /var/tmp
 TEMP_SUBDIR="/var/tmp/$0"
 
+# Name of final file
+FINAL_NAME="merged.txt"
+
 # URLs for the block lists
-LIST_ADS="https://blocklistproject.github.io/Lists/alt-version/ads-nl.txt"
-LIST_SCAM="https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt"
-LIST_REDIRECT="https://blocklistproject.github.io/Lists/alt-version/redirect-nl.txt"
-LIST_RANSOMWARE="https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt"
-LIST_PHISHING="https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt"
-LIST_MALWARE="https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt"
-LIST_FRAUD="https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt"
+LIST_BL_AD="https://blocklistproject.github.io/Lists/alt-version/ads-nl.txt"
+LIST_BL_SCAM="https://blocklistproject.github.io/Lists/alt-version/scam-nl.txt"
+LIST_BL_REDIRECT="https://blocklistproject.github.io/Lists/alt-version/redirect-nl.txt"
+LIST_BL_RANSOMWARE="https://blocklistproject.github.io/Lists/alt-version/ransomware-nl.txt"
+LIST_BL_PHISHING="https://blocklistproject.github.io/Lists/alt-version/phishing-nl.txt"
+LIST_BL_MALWARE="https://blocklistproject.github.io/Lists/alt-version/malware-nl.txt"
+LIST_BL_FRAUD="https://blocklistproject.github.io/Lists/alt-version/fraud-nl.txt"
 
 # Setup tmp dir in /var/tmp
 setup(){
-    mkdir -p "$TEMP_SUBDIR" || ( echo Could not create directory "'$TEMP_SUBDIR'" && exit 1 )
-    echo Setup complete.
+    ( mkdir -p "$TEMP_SUBDIR" && echo Setup complete. ) || ( echo Could not create directory "'$TEMP_SUBDIR'" && exit 1 )
 }
 
 # Gets list from the awesome github repo
 # Expects 1 arg: LIST_* var
 getlist(){
     cd "$TEMP_SUBDIR" || ( echo Could not change directory to "'$TEMP_SUBDIR'" && exit 1 )
+    ( fetch -q "$1"  && echo "'$1'" fetched successfully. ) || ( echo URL "'$1'" failed to get file && exit 1)
     
-    fetch -q "$1" || echo URL "'$1'" failed to get file && return
-    echo "'$1'" fetched successfully.
 }
 
 # Parses a file
 # Expects 1 argument: LIST_* variable
-parselist(){
+parselist_BL(){
     cd "$TEMP_SUBDIR" || ( echo Could not change directory to "'$TEMP_SUBDIR'" && exit 1 )
 
     filename="$(echo "$1" | awk -F/ '{print $NF}' )"
 
     # Remove lines after '#'
     sed -i '' 's/[\s]*#.*//g' "$filename"
+
+    # Remove any 0.0.0.0
+    sed -i '' 's/[\s]*0\.0\.0\.0[\s]*//g' "$filename"
 
     # Remove empty lines
     awk 'NF > 0' "$filename"> "$filename".tmp
@@ -56,34 +60,20 @@ parselist(){
     sed -i '' -e 's/\(.*\)/local-zone: "\1" refuse /' "$filename".tmp
 
     # If file is not empty, then continue
-    [ -s "$filename".tmp ] || ( echo File "$filename".tmp returned empty && exit 1 )
-
-    echo File "'$filename'" parsed successfully.
+    ( [ -s "$filename".tmp ] && echo File "'$filename'" parsed successfully. ) || ( echo File "$filename".tmp returned empty && exit 1 )   
 }
 
-# Copies list to the blocklist directory
-# Expects 1 arg: LIST_*
+# Moves final list to the blocklist directory
 copylist(){
-    filename="$(echo "$1" | awk -F/ '{print $NF}' )"
-    cp "$TEMP_SUBDIR/$filename.tmp" "$CONFIG_DIR/$filename.tmp" || ( echo Failed to copy "'$TEMP_SUBDIR/$filename.tmp'" to "'$CONFIG_DIR/$filename.tmp'" && exit 1 )
-}
-
-# Commits changes to unbound config
-# Expects 1 arg: LIST_*
-commitlist(){
-    cd "$CONFIG_DIR" || ( echo Could not change directory to "'$CONFIG_DIR'" && exit 1 )
-
-    filename="$(echo "$1" | awk -F/ '{print $NF}' )"
-    mv "$filename.tmp" "$filename"
+    cd "$TEMP_SUBDIR" || ( echo Could not change directory to "'$TEMP_SUBDIR'" && exit 1 )
+    sort -ubif ./*.tmp -o $FINAL_NAME || (echo Failed to sort files to "$FINAL_NAME" && exit 1)
+    cp -v "$TEMP_SUBDIR/$FINAL_NAME" "$CONFIG_DIR/." || ( echo Failed to copy "'$TEMP_SUBDIR/$FINAL_NAME'" to "'$CONFIG_DIR/'" && exit 1 )
 }
 
 # Removes lists in /var/tmp
 cleantmp(){
     cd /var/tmp || ( echo Could not change directory to /var/tmp && exit 1 )
-
-    rm "$TEMP_SUBDIR"/*
-    rmdir "$TEMP_SUBDIR"
-    echo Clean up process finished. Check for errors.
+    ( rm "$TEMP_SUBDIR"/* && rmdir "$TEMP_SUBDIR" && echo Clean up process finished. ) || ( echo Failed to clean "'$TEMP_SUBDIR'" && exit 1)
 }
 
 # Check unbound config for errors
@@ -94,61 +84,43 @@ servicecheck(){
 }
 
 # Revert changes if unbound config fails
-# Expects 1 arg: LIST_*
 revert(){
-    filename="$(echo "$1" | awk -F/ '{print $NF}' )"
     cd "$CONFIG_DIR" || ( echo Could not change directory to "'$CONFIG_DIR'" && exit 1 )
-    rm -v "$CONFIG_DIR/$filename.tmp"
+    rm -v "$CONFIG_DIR/$FINAL_NAME" || echo Could not remove "'$CONFIG_DIR/$FINAL_NAME'"
 }
 
 # Start
 setup
-getlist "$LIST_ADS"
-getlist "$LIST_SCAM"
-getlist "$LIST_REDIRECT"
-getlist "$LIST_RANSOMWARE"
-getlist "$LIST_PHISHING"
-getlist "$LIST_MALWARE"
-getlist "$LIST_FRAUD"
+getlist  "$LIST_BL_AD" &
+getlist "$LIST_BL_SCAM" &
+getlist "$LIST_BL_REDIRECT" & 
+getlist "$LIST_BL_RANSOMWARE" &
+getlist "$LIST_BL_PHISHING" &
+getlist "$LIST_BL_MALWARE" &
+getlist "$LIST_BL_FRAUD" &
 
-parselist "$LIST_ADS"
-parselist "$LIST_SCAM"
-parselist "$LIST_REDIRECT"
-parselist "$LIST_RANSOMWARE"
-parselist "$LIST_PHISHING"
-parselist "$LIST_MALWARE"
-parselist "$LIST_FRAUD"
+wait
 
-copylist "$LIST_ADS"
-copylist "$LIST_SCAM"
-copylist "$LIST_REDIRECT"
-copylist "$LIST_RANSOMWARE"
-copylist "$LIST_PHISHING"
-copylist "$LIST_MALWARE"
-copylist "$LIST_FRAUD"
+parselist_BL "$LIST_BL_AD" &
+parselist_BL "$LIST_BL_SCAM" &
+parselist_BL "$LIST_BL_REDIRECT" &
+parselist_BL "$LIST_BL_RANSOMWARE" &
+parselist_BL "$LIST_BL_PHISHING" &
+parselist_BL "$LIST_BL_MALWARE" &
+parselist_BL "$LIST_BL_FRAUD" &
 
+wait
+
+copylist
 servicecheck
 status=$?
 
 if [ $status -eq 0 ]; then
-    commitlist "$LIST_ADS"
-    commitlist "$LIST_SCAM"
-    commitlist "$LIST_REDIRECT"
-    commitlist "$LIST_RANSOMWARE"
-    commitlist "$LIST_PHISHING"
-    commitlist "$LIST_MALWARE"
-    commitlist "$LIST_FRAUD"
     unbound-control reload && echo Unbound service restarted
     cleantmp
     unbound-control status
 else
     echo Will revert changes. Temp files will be kept for inspection.
-    revert "$LIST_ADS"
-    revert "$LIST_SCAM"
-    revert "$LIST_REDIRECT"
-    revert "$LIST_RANSOMWARE"
-    revert "$LIST_PHISHING"
-    revert "$LIST_MALWARE"
-    revert "$LIST_FRAUD"
+    revert
     echo Revert complete.
 fi
